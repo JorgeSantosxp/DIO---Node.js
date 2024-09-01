73ebd9a2f1938939f8ac984dd2cb00c07aff965c
JorgeSantosxp <jorgesantos.xp@gmail.com>
2024-09-01 18:26:34 -0300

    API da Champions League com Node.js e Express

---
+++ projeto-champions/src/services/players-services.ts
@@ -0,0 +1,74 @@
+import { response } from "express";
+import * as PlayerRepository from "../repositories/players-repository";
+import * as HttpResponse from "../utils/http-helper";
+import { PlayerModel } from "../models/player-model";
+import { StatisticsModel } from "../models/statistic-model";
+
+
+export const getPlayerService = async () => {
+    const data = await PlayerRepository.findAllPlayers();
+    let response = null;
+
+    if(data) {
+        response = await HttpResponse.ok(data);
+    }else{
+        response = await HttpResponse.noContent();
+    }
+
+    return response;
+};
+
+export const getPlayerByIdService = async(id: number) => {
+    //pedir para o repositório de dados
+    const data = await PlayerRepository.findPlayerById(id);
+    let response = null;
+
+    if(data) {
+        response = await HttpResponse.ok(data);
+    }else{
+        response = await HttpResponse.noContent();
+    }
+
+    return response;
+};
+
+export const createPlayerService = async (player: PlayerModel) => {
+    let response = null;
+    // verifica se está vazio o objeto
+
+    if(Object.keys(player).length !== 0) {
+        await PlayerRepository.insertPlayer(player);
+        response = await HttpResponse.created();
+    }else{
+        response = await HttpResponse.badRequest();
+    }
+
+    return response;
+};
+
+export const deletePlayerService = async (id: number) => {
+    let response = null;
+    const isDelete = await PlayerRepository.deleteOnePlayer(id);
+    
+    if(isDelete) {
+        response = await HttpResponse.ok({messege: "deleted"});
+    }else{
+        response = await HttpResponse.badRequest();        
+    }
+    return response;
+}
+
+export const updatePlayerService = async (
+    id: number, 
+    statistics: StatisticsModel
+) => {
+    const data = await PlayerRepository.findAndModifyPlayer(id, statistics);
+    let response = null;
+    if(Object.keys(data).length === 0) {
+        response = await HttpResponse.badRequest();
+    }else{
+        response = await HttpResponse.ok(data);
+    }
+
+    return response;
+};
